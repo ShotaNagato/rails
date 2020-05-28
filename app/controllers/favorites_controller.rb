26bd42a8f2ea31bac8622485f5bcdcbfef2538a7
Shota <shota.nagato@gmail.com>
2020-05-28 09:39:44 +0000

    いいね一覧

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,7 +9,7 @@ class FavoritesController < ApplicationController
     favorite.topic_id = params[:topic_id]
     
     if favorite.save
-      redirect_to topic_path, success: 'お気に入りに登録しました'
+      redirect_to topics_path, success: 'お気に入りに登録しました'
       
     else
       redirect_to topics_path, danger: 'お気に入りに登録に失敗しました'
