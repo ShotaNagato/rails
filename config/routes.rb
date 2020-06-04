8121ca91291031398fff3ad033ad841423d6c0b7
Shota <shota.nagato@gmail.com>
2020-06-04 03:01:20 +0000

    いいね取り消し

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,14 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
+  
 
   resources :users
   resources :topics
   
   get 'favorites/index'
   post '/favorites', to: 'favorites#create'
+  delete '/favorites', to: 'favorites#destroy'
+
 end
